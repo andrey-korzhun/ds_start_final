d1c0af9558b800baa89140455d3d73bb17bcfca4
Andrey Korzhun <steelcon@bk.ru>
2024-04-28 13:13:05 +0300

    fix

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 import streamlit as st
 import pandas as pd
 
+import re
+import plotly
+
+import plotly.graph_objects as go
+import plotly.express as px
+
+from plotly.subplots import make_subplots
+
 
 st.set_page_config('Проект: анализ зарплат в России', layout='wide', initial_sidebar_state='auto')
 st.subheader('Данные об инфляции')
@@ -22,5 +30,77 @@ old_data = old_data.set_axis(['Отрасль'] + [str(x) for x in range(2000, 2
 new_data = new_data.dropna(how='any')
 old_data = old_data.dropna(how='any')
 
-st.write(new_data.head())
 st.write(old_data.head())
+st.write(new_data.head())
+
+infl = df_infl.loc[df_infl.index >= 2000 , 'Всего'][1:].sort_index()
+infl.name = 'Инфляция'
+
+def sanitaze_str(str):
+    str = str.strip().capitalize()
+    str = re.sub(r'\s{2,}', ' ', str)
+    if str in col_name_mapping:
+        str = col_name_mapping[str]
+    return str
+
+
+def discount(year_from, year_to, sum):
+    result = sum
+    for x in range(year_from, year_to, -1):
+        result /= 1.0 + infl.loc[x] / 100.0
+    return result
+
+
+def compound(year_from, year_to, sum):
+    result = sum
+    for x in range(year_from, year_to):
+        result *= 1.0 + infl.loc[x] / 100.0
+    return result
+
+
+def get_line(data, name):
+    line = data[data['Отрасль'] == name].drop(['Отрасль'], axis=1)
+    line = line.squeeze()
+    line.index = line.index.map(int)
+    return line
+
+
+col_name_mapping = {
+    'Рыболовство, рыбоводство': 'Рыболовство и рыбоводство',
+    'Производство кожи, изделий из кожи и производство обуви': 'Производство кожи и изделий из кожи',
+    'Производство резиновых и пластмассовых изделий': 'Производство резиновых и пластмассовых изделий',
+    'Торговля оптовая и розничная; ремонт автотранспортных средств и мотоциклов': 'Оптовая и розничная торговля; ремонт автотранспортных средств, мотоциклов, бытовых изделий и предметов личного пользования',
+    'Деятельность финансовая и страховая': 'Финансовая деятельность',
+    'Государственное управление и обеспечение военной безопасности; социальное обеспечение': 'Государственное управление и обеспечение военной безопасности; социальное страхование',
+    'Деятельность в области здравоохранения и социальных услуг': 'Здравоохранение и предоставление социальных услуг',
+    'Всего': 'Средняя',
+    'Всего по экономике': 'Средняя',
+}
+
+new_data['Отрасль'] = new_data['Отрасль'].apply(sanitaze_str)
+old_data['Отрасль'] = old_data['Отрасль'].apply(sanitaze_str)
+data = old_data.merge(new_data, on='Отрасль')
+
+
+data_2000 = data.copy()
+
+for year in infl.index:
+    data_2000[str(year)] = discount(year, 2000, data_2000[str(year)])
+
+color, colors = 0, plotly.colors.qualitative.Plotly
+fig = go.Figure()
+# выбранные отрасли + среднее по всем отраслям
+lines = ['Образование', 'Финансовая деятельность', 'Строительство', 'Средняя']
+
+for name in lines:
+    dt = get_line(data, name)
+    dt_real = get_line(data_2000, name)
+    fig.add_trace(go.Scatter(x=dt_real.index, y=dt_real.array, name=name + ' с учетом инфл.', line=dict(color=colors[color])))
+    fig.add_trace(go.Scatter(x=dt.index, y=dt.array, name=name, line=dict(dash='dot', color=colors[color])))
+    color += 1
+
+fig.update_layout(xaxis_title='год', yaxis_title='з/п, руб.', yaxis_type='log',
+                  margin=dict(l=20,r=20,b=10,t=40))
+
+
+st.plotly_chart(fig, use_container_width=True)
